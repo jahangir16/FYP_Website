fb237300f1bf789847ecd464739c756fc52d1399
jahangir16 <kamaljahangir13@gmail.com>
2024-05-13 02:01:12 +0500

    i now know how to utlized the auth and roles

--- FYP_Website/app/routes/product.routes.js
+++ FYP_Website/app/routes/product.routes.js
@@ -2,6 +2,14 @@
 
 // Initilize express router
 module.exports = app => {
+    app.use(function(req, res, next) {
+        res.header(
+          "Access-Control-Allow-Headers",
+          "x-access-token, Origin, Content-Type, Accept"
+        );
+        next();
+      });
+
     const products = require("../controllers/product.controller.js");
   
     var router = require("express").Router();
@@ -10,9 +18,10 @@ module.exports = app => {
     router.get("/products", products.getAllProducts);
 
     // Retrieve a specific product with reviews
-    // Use the saveUser middleware for authentication
-    router.get("/product/:id", products.getProductWithReviews);
+    //router.get("/product/:id", products.getProductWithReviews);
+    const { authJwt } = require("../middleware"); // adjust the path as needed
 
+    router.get("/product/:id", [authJwt.verifyToken, authJwt.isModerator], products.getProductWithReviews);
     // Use the router
     app.use('/', router);
 };
