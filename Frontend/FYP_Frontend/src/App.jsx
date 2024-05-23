b76e09a5cdc719d09a18170c2011b8b5a0469c40
jahangir16 <kamaljahangir13@gmail.com>
2024-05-23 18:31:15 +0500

    done

--- Frontend/FYP_Frontend/src/App.jsx
+++ Frontend/FYP_Frontend/src/App.jsx
@@ -3,10 +3,12 @@ import ProductCard from './components/ProductCard';
 import ProductDetails from './components/ProductDetails';
 import { Route, BrowserRouter, Routes } from 'react-router-dom'
 
+
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        
         <Route path="/" element={<ProductCard />} />
         <Route path="/product/:productId" element={<ProductDetails />} />
         {/* Other routes */}
